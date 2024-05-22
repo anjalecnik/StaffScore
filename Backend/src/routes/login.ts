779a2484cffe435f14bba2cd4e6770ccf7984840
Anja Lečnik <anjas.lecnik@gmail.com>
2024-05-22 20:27:42 +0200

    resolved #1 Google Auth login
    Feature: Enable users to log in via Google Authentication.

---
+++ Backend/src/routes/login.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import { db } from "../config/firebase";
+import { getDocs, collection, query, where } from "firebase/firestore";
+
+const router = Router();
+
+router.post("/check-user", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("email", "==", email));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      const userData = querySnapshot.docs[0].data();
+      const userId = querySnapshot.docs[0].id;
+
+      res.json({ userData, userId });
+    } else {
+      res
+        .status(404)
+        .json({ message: "No user found with the email: " + email });
+    }
+  } catch (error) {
+    console.error("Error getting user by email:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export default router;
